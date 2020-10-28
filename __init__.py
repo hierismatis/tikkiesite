d5125ca8f58b000f2fc9d83d5fa12ccd05cde65c
hierismatis <matthijsdingemanse@outlook.com>
2020-10-28 09:42:04 +0100

    finished the 'addtikkie' post, and added a pay endpoint to the api

--- __init__.py
+++ __init__.py
@@ -19,22 +19,29 @@ changepass_put_args.add_argument("newpass", type=str, help="New/future login pas
 changepass_put_args.add_argument("newpass1", type=str, help="Duplicate of newpass", required=True)
 
 addtikkie_put_args = reqparse.RequestParser()
-addtikkie_put_args.add_argument("name", type=str, help="What is the tikkie for?", required=True)
+addtikkie_put_args.add_argument("name", type=str, help="Login name", required=True)
+addtikkie_put_args.add_argument("password", type=str, help="Login password", required=True)
+addtikkie_put_args.add_argument("tikkiename", type=str, help="What is the tikkie for?", required=True)
 addtikkie_put_args.add_argument("url", type=str, help="The url of the tikkie", required=True)
 addtikkie_put_args.add_argument("payers", type=str, help="People who have to pay", required=True)
 addtikkie_put_args.add_argument("date", type=str, help="The date the money was spend", required=True)
+
+pay_post_args = reqparse.RequestParser()
+pay_post_args.add_argument("name", type=str, help="Login name", required=True)
+pay_post_args.add_argument("password", type=str, help="Login password", required=True)
+pay_post_args.add_argument("tikkieid", type=str, help="Id of the tikkie you paid", required=True)
+
 
 class LogIn(Resource):
 	def post(self):
 		args = login_post_args.parse_args()
 		name, password = args["name"], args["password"]
 
-		user_data = get_data("users")
-		user_info = user_data[name]
-
 		if not check_login(name, password):
 			return {"Authorization error": "Bad credentials"}, 403
 
+		user_data = get_data("users")
+		user_info = user_data[name]
 		return user_info, 200
 
 class ChangePassword(Resource):
@@ -57,13 +64,77 @@ class ChangePassword(Resource):
 class AddTikkie(Resource):
 	def put(self):
 		args = addtikkie_put_args.parse_args()
-		name, url, payers, date = args["name"], args["url"], args["payers"].split(","), args["date"]
-		print(payers)
-		return payers
+		name, password, t_name, t_url, t_payers, t_date = args["name"], args["password"], args["tikkiename"], args["url"], args["payers"].split(","), args["date"]
+
+
+		tikkies = get_data("tikkies")
+		user_data = get_data("users")
+
+		if not check_login(name, password):
+			return {"Authorization error": "Bad credentials"}, 403
+
+		if set(t_payers) & set(user_data.keys()) != set(t_payers):
+			return {"Bad request": "Not all payers are users"}, 400
+
+		if len(tikkies) != 0:
+			t_id = tikkies[len(tikkies)-1]["id"]+1
+		else:
+			t_id = 0
+
+		tikkie = {
+		"id": t_id,
+		"creator": name,
+		"name": t_name,
+		"url": t_url,
+		"unpaid": t_payers,
+		"paid": [],
+		"date": t_date
+		}
+
+		tikkies.append(tikkie)
+		put_data("tikkies", tikkies)
+
+		for payer in t_payers:
+			user_data[payer]["tikkies"].append(t_id)
+		
+		put_data("users", user_data)
+
+		return {"Tikkie added": "Succes"}, 201
+
+class Payed(Resource):
+	def post(self):
+		args = pay_post_args.parse_args()
+		name, password, t_id = args["name"], args["password"], args["tikkieid"]
+
+		user_data = get_data("users")
+		tikkies = get_data("tikkies")
+
+		if not check_login(name, password):
+			return {"Authorization error": "Bad credentials"}, 403
+
+		try:
+			t_id = int(t_id)
+		except ValueError:
+			return {"Error": "Tikkie_id not convertable to int"}, 400
+
+		if name not in tikkies[t_id]["unpaid"]:
+			return {"Exception": "Tikkie already paid!"}, 202
+
+		tikkies[t_id]["unpaid"].remove(name)
+		tikkies[t_id]["paid"].append(name)
+
+		user_data[name]["tikkies"].remove(t_id)
+		user_data[name]["paid"].append(t_id)
+
+		put_data("tikkies", tikkies)
+		put_data("users", user_data)
+		
+		return {"Tikkie paid": "Succes"}
 
 api.add_resource(LogIn, "/login")
 api.add_resource(ChangePassword, "/changepass")
 api.add_resource(AddTikkie, "/addtikkie")
+api.add_resource(Payed, "/pay")
 
 if __name__ == "__main__":
 	app.run(debug=True)

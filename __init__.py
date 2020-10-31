7bb848019815a473bd2406b267c756b68b48364b
Aron Rijken <acrijken@tutanota.com>
2020-10-31 12:42:42 +0100

    Add SQLite support

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
 from backendfunctions import *
-import json
-import hashlib
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "6810527e5f4636beca705e70625e96bc"
@@ -24,25 +24,40 @@ addtikkie_put_args.add_argument("password", type=str, help="Login password", req
 addtikkie_put_args.add_argument("tikkiename", type=str, help="What is the tikkie for?", required=True)
 addtikkie_put_args.add_argument("url", type=str, help="The url of the tikkie", required=True)
 addtikkie_put_args.add_argument("payers", type=str, help="People who have to pay", required=True)
-addtikkie_put_args.add_argument("date", type=str, help="The date the money was spend", required=True)
 
 pay_post_args = reqparse.RequestParser()
 pay_post_args.add_argument("name", type=str, help="Login name", required=True)
 pay_post_args.add_argument("password", type=str, help="Login password", required=True)
 pay_post_args.add_argument("tikkieid", type=str, help="Id of the tikkie you paid", required=True)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50), unique=True)
+	password = db.Column(db.String(100))
+
+class Tikkie(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	description = db.Column(db.String(200))
+	creator = db.Column(db.String(50), db.ForeignKey("user.name"))
+	url = db.Column(db.String(50), unique=True)
+	date = db.Column(db.DateTime, default=datetime.now)
+	paid = db.Column(db.String(300), default="")
+	unpaid = db.Column(db.String(300))
 
 class LogIn(Resource):
 	def post(self):
 		args = login_post_args.parse_args()
-		name, password = args["name"], args["password"]
+		name, password = args["name"], password_hasher(args["password"])
 
-		if not check_login(name, password):
+		user = User.query.filter_by(name=name).filter_by(password=password).first()
+
+		if user is None:
 			return {"Authorization error": "Bad credentials"}, 403
-
-		user_data = get_data("users")
-		user_info = user_data[name]
-		return user_info, 200
+		return json.dumps(dict(name=user.name, password=user.password)), 200
 
 class ChangePassword(Resource):
 	def put(self):
@@ -50,66 +65,43 @@ class ChangePassword(Resource):
 		name, oldpass, newpass, newpass1 = args["name"], args["oldpass"], args["newpass"], args["newpass1"]
 
 		if newpass != newpass1:
-			return {"Authorization error": "New passwords don't are not the same!"}, 403
+			return {"Authorization error": "New passwords are not the same!"}, 403
 
-		if not check_login(name, oldpass):
+		user = User.query.filter_by(name=name).filter_by(password=password_hasher(oldpass)).first()
+
+		if user is None:
 			return {"Authorization error": "Bad credentials"}, 403
 
-		user_data = get_data("users")
-		user_data[name]["password"] = password_hasher(newpass)
-		put_data("users", user_data)
+		user.password = password_hasher(newpass)
+		db.session.commit()
 
 		return {"Password change": "Succes"}, 200
 
 class AddTikkie(Resource):
 	def put(self):
 		args = addtikkie_put_args.parse_args()
-		name, password, t_name, t_url, t_payers, t_date = args["name"], args["password"], args["tikkiename"], args["url"], args["payers"].split(","), args["date"]
+		name, password, t_name, t_url, t_payers = args["name"], args["password"], args["tikkiename"], args["url"], args["payers"].split(",")
 
-
-		tikkies = get_data("tikkies")
-		user_data = get_data("users")
-
-		if not check_login(name, password):
+		if User.query.filter_by(name=name).filter_by(password=password_hasher(password)).first() is None:
 			return {"Authorization error": "Bad credentials"}, 403
 
-		if set(t_payers) & set(user_data.keys()) != set(t_payers):
+		users = map(lambda user: user.name, User.query.all())
+
+		if set(t_payers) & set(users) != set(t_payers):
 			return {"Bad request": "Not all payers are users"}, 400
 
-		if len(tikkies) != 0:
-			t_id = tikkies[len(tikkies)-1]["id"]+1
-		else:
-			t_id = 0
+		tikkie = Tikkie(creator=name, description=t_name, url=t_url, unpaid=",".join(t_payers))
+		db.session.add(tikkie)
+		db.session.commit()
 
-		tikkie = {
-		"id": t_id,
-		"creator": name,
-		"name": t_name,
-		"url": t_url,
-		"unpaid": t_payers,
-		"paid": [],
-		"date": t_date
-		}
-
-		tikkies.append(tikkie)
-		put_data("tikkies", tikkies)
-
-		for payer in t_payers:
-			user_data[payer]["tikkies"].append(t_id)
-		
-		put_data("users", user_data)
-
-		return {"Tikkie added": "Succes"}, 201
+		return {"success": True, "id": tikkie.id}, 201
 
 class Payed(Resource):
 	def post(self):
 		args = pay_post_args.parse_args()
 		name, password, t_id = args["name"], args["password"], args["tikkieid"]
 
-		user_data = get_data("users")
-		tikkies = get_data("tikkies")
-
-		if not check_login(name, password):
+		if User.query.filter_by(name=name).filter_by(password=password_hasher(password)).first() is None:
 			return {"Authorization error": "Bad credentials"}, 403
 
 		try:
@@ -117,17 +109,24 @@ class Payed(Resource):
 		except ValueError:
 			return {"Error": "Tikkie_id not convertable to int"}, 400
 
-		if name not in tikkies[t_id]["unpaid"]:
+		tikkie = Tikkie.query.filter_by(id=t_id).first()
+
+		if tikkie is None:
+			return {"Error": "Tikkie not found"}, 400
+
+		paid = tikkie.paid.split(",")
+		unpaid = tikkie.unpaid.split(",")
+
+		if name not in unpaid:
 			return {"Exception": "Tikkie already paid!"}, 202
 
-		tikkies[t_id]["unpaid"].remove(name)
-		tikkies[t_id]["paid"].append(name)
+		paid.append(name)
+		tikkie.paid = ",".join(paid)
 
-		user_data[name]["tikkies"].remove(t_id)
-		user_data[name]["paid"].append(t_id)
+		unpaid.remove(name)
+		tikkie.unpaid = ",".join(unpaid)
 
-		put_data("tikkies", tikkies)
-		put_data("users", user_data)
+		db.session.commit()
 		
 		return {"Tikkie paid": "Succes"}
 
